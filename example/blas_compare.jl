0eb13e22298c286607adfb4f3679d3be8e8ec40d
Mark Agerton (UCD - Gitkraken) <mjagerton@ucdavis.edu>
2018-09-14 14:26:17 -0700

    first commit

---
+++ example/blas_compare.jl
@@ -0,0 +1,29 @@
+using testModule
+using LinearAlgebra
+using BenchmarkTools
+using Random
+
+Random.seed!(1234)
+
+trial_k = 2:10:52
+
+# for k in trial_k
+#     y = zeros(Float64, k)
+#     A = rand(Float64, k,k)
+#     x = rand(Float64, k)
+#
+#     println("Benchmarking gemv! for $k...")
+#     println("gemv!")
+#     @btime mygemv!($y, $A, $x)
+#     @btime BLAS.gemv!('N', 1.0, $A, $x, 1.0, $y)
+# end
+
+for k in trial_k
+    y = zeros(Float64, k)
+    A = rand(Float64, k,k)
+    x = rand(Float64, k)
+
+    println("Benchmarking ger! for $k...")
+    @btime myger!($A, 2.0, $x, $x)
+    @btime BLAS.ger!(2.0, $x, $x, $A)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,30 @@
-using Distributed
-using testModule
+# using Distributed
+# using testModule
+#
+# pid = addprocs()
+# @everywhere using testModule
+#
+# @show testModule.testfun()
 
-@everywhere using testModule
+using Random
+using LinearAlgebra
 
-testfun()
+Random.seed!(1234)
+
+x = rand(5,5)
+eyemat = Matrix{Float64}(I,size(x))
+
+xinv1 = x \ I
+xinv2 = x \ eyemat
+xinv3 = inv(x)
+
+inv(x) == inv(lu(x))
+lu(x) \ eyemat == x \ eyemat
+
+@which x \ I
+@which x \ eyemat
+@which inv(x)
+
+xinv1 .- xinv2
+xinv2 .- xinv3
+xinv1 .- xinv3
