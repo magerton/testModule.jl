57856f3b023461bed98c79eee247a051ba8ad60d
Mark <mjagerton@ucdavis.edu>
2022-03-31 19:35:41 -0700

    Update TestModule.jl

--- src/TestModule.jl
+++ src/TestModule.jl
@@ -20,15 +20,40 @@ abstract type AbstractObservationGroup{N}  <: AbstractLeveledDataObject{N} end
 
 Level(::AbstractLeveledDataObject{N}) where {N} = N
 
-struct ObservationGroup{D,I}
-    d::D
+struct ObservationGroup{N,D,I} <: AbstractObservationGroup{N}
+    data::D
     idx::I
-    function ObservationGroup(d,i)
+    function ObservationGroup(d,idx)
         N = Level(d)
-        new
+        return new{N-1,D,I}(d,idx)
     end
 end
 
+data(x::AbstractObservation) = x.data
+idx(x::AbstractObservation) = x.idx
+
+struct Data{N,Int}
+    y::Vector{Float64}
+    x::Matrix{Float64}
+    ptrs::NTuple{N,Int}
+    function Data(y,x,ptrs::NTuple{N}) where {N}
+        k,n = size(x)
+        length(y) == n || throw(DimensionMismatch())
+        if N > 0
+            @assert all(first.(ptrs) .== 1)
+            lastptrs = last.(ptrs) .- 1
+            @assert last(lastptrs) == n
+            if N > 1
+            end
+        end
+    end
+end
+
+getindex(d::AbstractLeveledDataObject{0},i) = Observation(d,i)
+getindex(d::AbstractLeveledDataObject,i)    = ObservationGroup(d,i)
+
+
+Abs
 
 
 end # module
