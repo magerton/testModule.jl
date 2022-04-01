fd6b4535467209232923a7d70819ab877a49b6a0
Mark A SFF <mjagerton@ucdavis.edu>
2022-03-31 22:18:17 -0700

    messing around to simplify stuff

--- src/TestModule.jl
+++ src/TestModule.jl
@@ -12,48 +12,83 @@ using Base: OneTo
 abstract type AbstractDataObject end
 
 abstract type AbstractTmpVar{R} <: AbstractDataObject end
+(::Type{T})(x::AbstractTmpVar) where {T<:AbstractTmpVar} = x
 
-abstract type AbstractObservation          <: AbstractDataObject end
-abstract type AbstractLeveledDataObject{N} <: AbstractDataObject end
-abstract type AbstractData{N}              <: AbstractLeveledDataObject{N} end
-abstract type AbstractObservationGroup{N}  <: AbstractLeveledDataObject{N} end
+abstract type AbstractData       <: AbstractDataObject end
+getindex(d::AbstractData,i) = ObservationGroup(d,i)
 
-Level(::AbstractLeveledDataObject{N}) where {N} = N
 
-struct ObservationGroup{N,D,I} <: AbstractObservationGroup{N}
+
+abstract type AbstractDataSet           <: AbstractData end
+abstract type AbstractObservationGroup  <: AbstractData end
+abstract type AbstractObservation       <: AbstractData end
+
+struct DataWithTmpVar{D<:AbstractData, T<:AbstractTmpVar} <: AbstractData
+    data::D
+    tmpvar::T
+end
+
+struct ObservationGroup{D,I} <: AbstractObservationGroup
     data::D
     idx::I
-    function ObservationGroup(d,idx)
-        N = Level(d)
-        return new{N-1,D,I}(d,idx)
-    end
 end
 
-data(x::AbstractObservation) = x.data
-idx(x::AbstractObservation) = x.idx
+struct Observation{YT,XT} <: AbstractObservation
+    y::YT
+    x::XT
+end
 
-struct Data{N,Int}
+y(d::AbstractData) = d.y
+x(d::AbstractData) = d.x
+
+ptr(   d::AbstractDataSet) = d.ptr
+length(d::AbstractDataSet)  = length(ptr(d))
+
+data(x::AbstractObservationGroup) = x.data
+idx( x::AbstractObservationGroup) = x.idx
+
+
+struct DataSingle <: AbstractDataSet
     y::Vector{Float64}
     x::Matrix{Float64}
-    ptrs::NTuple{N,Int}
-    function Data(y,x,ptrs::NTuple{N}) where {N}
+    function DataSingle(y,x)
         k,n = size(x)
-        length(y) == n || throw(DimensionMismatch())
-        if N > 0
-            @assert all(first.(ptrs) .== 1)
-            lastptrs = last.(ptrs) .- 1
-            @assert last(lastptrs) == n
-            if N > 1
-            end
-        end
+        n == length(y) == length(ptr) || throw(DimensionMismatch())
+        first(ptr) == 1 || throw(error())
+        last(ptr)-1 == n || throw(error())
+        return new(y,x,ptr)
     end
 end
 
-getindex(d::AbstractLeveledDataObject{0},i) = Observation(d,i)
-getindex(d::AbstractLeveledDataObject,i)    = ObservationGroup(d,i)
+ptr(d::DataSingle) = OneTo(length(y(d)))
+
+struct DataMult <: AbstractDataSet
+    y::Vector{Float64}
+    x::Matrix{Float64}
+    ptr::Vector{Int}
+    function DataMult(y,x,ptr)
+        k,n = size(x)
+        n == length(y) == length(ptr) || throw(DimensionMismatch())
+        first(ptr) == 1 || throw(error())
+        last(ptr)-1 == n || throw(error())
+        return new(y,x,ptr)
+    end
+end
 
 
-Abs
+
+function Observation(d::DataSingle, i) 
+    yi = getindex(y(d), i)
+    xi = view(x(d), :, i)
+    return Observation(yi, xi)
+end
+
+function Observation(d::Datamult, i) 
+    yi = view(y(d), i)
+    xi = view(x(d), :, i)
+    return Observation(yi, xi)
+end
+
 
 
 end # module
