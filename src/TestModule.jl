20e1752b628ba4a7ef2fff928e790bb38306c083
Mark A SFF <mjagerton@ucdavis.edu>
2022-04-01 10:45:13 -0700

    working on draft for how iteration should work

--- src/TestModule.jl
+++ src/TestModule.jl
@@ -14,34 +14,52 @@ abstract type AbstractDataSet           <: AbstractData end
 abstract type AbstractObservationGroup  <: AbstractData end
 abstract type AbstractObservation       <: AbstractData end
 
+"""
+we give a liklihood evaluation a DataSet/ObsGroup/Observation
+object with a `AbstractTmpVar`
+"""
 struct DataWithTmpVar{D<:AbstractData, T<:AbstractTmpVar} <: AbstractData
     data::D
     tmpvar::T
 end
 
+"wrap a DataSet/DataWithTmpVar/ObsGroup/Observation with an index"
 struct ObservationGroup{D,I} <: AbstractObservationGroup
     data::D
     idx::I
 end
 
+"Observation we use inside a likelihood"
 struct Observation{YT,XT} <: AbstractObservation
     y::YT
     x::XT
 end
 
-struct DataSingle <: AbstractDataSet
+struct TmpVar{R} <: AbstractTmpVar
+    xbeta::Vector{R}
+end 
+
+abstract type AbstractObservationType end
+struct ScalarObs end
+struct VectorObs end
+
+"default to scalar"
+obstype(::AbstractDataSet) = ScalarObs()
+
+"An Observation/ObsGroup is just a scalar"
+struct DataScalar <: AbstractDataSet
     y::Vector{Float64}
     x::Matrix{Float64}
     function DataSingle(y,x)
         k,n = size(x)
-        n == length(y) == length(ptr) || throw(DimensionMismatch())
-        first(ptr) == 1 || throw(error())
-        last(ptr)-1 == n || throw(error())
-        return new(y,x,ptr)
+        n == length(y)
+        return new(y,x)
     end
 end
 
-struct DataMult <: AbstractDataSet
+
+"An Observation/ObsGroup is a vector"
+struct DataVector <: AbstractDataSet
     y::Vector{Float64}
     x::Matrix{Float64}
     ptr::Vector{Int}
@@ -54,9 +72,126 @@ struct DataMult <: AbstractDataSet
     end
 end
 
-struct TmpVar{R} <: AbstractTmpVar
-    xbeta::Vector{R}
-end 
+obstype(::DataVector) = VectorObs()
+
+"An ObsGroup is a vector... but we have to go over Observations as scalars"
+struct DataVectorByScalar <: AbstractDataSet
+    y::Vector{Float64}
+    x::Matrix{Float64}
+    ptr::Vector{Int}
+    function DataDrill(y,x,ptr)
+        k,n = size(x)
+        n == length(y) == length(ptr) || throw(DimensionMismatch())
+        first(ptr) == 1 || throw(error())
+        last(ptr)-1 == n || throw(error())
+        return new(y,x,ptr)
+    end
+end
+
+y(d::AbstractData) = d.y
+x(d::AbstractData) = d.x
+
+nobs(d::AbstractData) = length(y(d))
+length(d::DataScalar) = length(y(d))
+ptr(d::DataScalar) = OneTo(length(y(d)))
+
+function Observation(d::DataScalar, i)
+    yi = y(d)[i]
+    xi = view(x(d),i)
+    Observation
+end
+
+getorview(d::DataSingle,i) = getindex(y(d), i)
+getorview(d::DataMult,i)   = view(    y(d), i)
+
+"spits out data for observation `i`, be it length 1 or length n"
+function Observation(d::AbstractDataSet, i) 
+    yi = getorview(d, i)
+    xi = view(x(d), :, i)
+    return Observation(yi, xi)
+end
+
+
+
+
+
+
+# ObservationGroup
+length(o::AbstractObservation) = length(_y(o))
+
+# default methods for iteration through an AbstractDataStructure
+firstindex(d::AbstractDataStructure) = 1
+lastindex( d::AbstractDataStructure) = length(d)
+IndexStyle(d::AbstractDataStructure) = IndexLinear()
+eachindex( d::AbstractDataStructure) = OneTo(length(d))
+
+# Default iteration method is to create an ObservationGroup
+getindex(  d::AbstractDataStructure, i) = ObservationGroup(d,i)
+
+function iterate(d::AbstractDataStructure, i=firstindex(d))
+    if i <= lastindex(d)
+        return getindex(d,i), i+1
+    else
+        return nothing
+    end
+end
+
+# AbstractDataSet iteration utilties
+#------------------------------------------
+
+_data(d::AbstractDataSet) = d
+
+group_ptr(d::AbstractDataSet) = throw(error("group_ptr not defined for $(typeof(d))"))
+obs_ptr(  d::AbstractDataSet) = throw(error("obs_ptr not defined for $(typeof(d))"))
+
+# default method
+length(   d::AbstractDataSet) = length(group_ptr(d))-1
+_num_obs( d::AbstractDataSet) = length(obs_ptr(d))-1
+
+groupstart( d::AbstractDataSet, i::Integer) = getindex(group_ptr(d), i)
+groupstop(  d::AbstractDataSet, i::Integer) = groupstart(d,i+1)-1
+grouplength(d::AbstractDataSet, i::Integer) = groupstop(d,i) - groupstart(d,i) + 1
+grouprange( d::AbstractDataSet, i::Integer) = groupstart(d,i) : groupstop(d,i)
+
+obsstart( d::AbstractDataSet, j::Integer) = getindex(obs_ptr(d),j)
+obsstop(  d::AbstractDataSet, j::Integer) = obsstart(d,j+1)-1
+obslength(d::AbstractDataSet, j::Integer) = obsstop(d,j) - obsstart(d,j) + 1
+obsrange( d::AbstractDataSet, j::Integer) = obsstart(d,j) : obsstop(d,j)
+
+# DataSet or Observation
+_model(d::DataOrObs) = d.model
+_y(    d::DataOrObs) = d.y
+_x(    d::DataOrObs) = d.x
+_num_x(d::DataOrObs) = size(_x(d), 1)
+
+# ObservationGroup iteration utilties
+#------------------------------------------
+
+_data( g::AbstractObservationGroup) = g.data
+_i(    g::AbstractObservationGroup) = g.i
+_num_x(g::AbstractObservationGroup) = _num_x(_data(g))
+_nparm(g::AbstractObservationGroup) = _nparm(_data(g))
+_model(g::AbstractObservationGroup) = _model(_data(g))
+
+length(    g::AbstractObservationGroup) = grouplength(_data(g), _i(g))
+grouprange(g::AbstractObservationGroup) = grouprange( _data(g), _i(g))
+
+obsstart( g::AbstractObservationGroup, k) = obsstart( _data(g), getindex(grouprange(g), k))
+obsrange( g::AbstractObservationGroup, k) = obsrange( _data(g), getindex(grouprange(g), k))
+obslength(g::AbstractObservationGroup, k) = obslength(_data(g), getindex(grouprange(g), k))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 (::Type{T})(x::AbstractTmpVar) where {T<:AbstractTmpVar} = x
 
@@ -66,7 +201,7 @@ y(d::AbstractData) = d.y
 x(d::AbstractData) = d.x
 
 ptr(d::AbstractDataSet) = d.ptr
-ptr(d::DataSingle) = OneTo(length(y(d)))
+ptr(d::DataSingle) = nothing
 length(d::AbstractDataSet)  = length(ptr(d))
 length(o::Observation) = length(y(o))
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,83 +5,66 @@ module CompileTestModule
 end # module
 
 
-# module TestModule_Test
-using Test
-using TestModule
-using TestModule: tmpvar, y, x,
-    obsrange, grouprange,
-    group_ptr, obs_ptr,
-    TmpVarTest,
-    xbeta
+# # module TestModule_Test
+# using Test
+# using TestModule
+# using TestModule: tmpvar, y, x,
+#     obsrange, grouprange,
+#     group_ptr, obs_ptr,
+#     TmpVarTest,
+#     xbeta
 
-# @testset "DataTest" begin
-    k,n = 2,4
-    d = DataTest(rand(n), rand(k,n))
 
-    for (i,o) in enumerate(d)
-        @test o isa  AbstractObservation
-        @test isa(o, AbstractObservationGroup) == false
+for data in (datascalar, datascalar_wtmpvar)
+    for (i,grp) in data
+        obs = Observation(grp)
+        @test obs == Observation(data,i)
+        @test y(obs) isa Number
+        @test y(obs) === y(data)[i]
+        @test x(obs) isa SubArray
+        @test x(obs) === view(x(data), :, i)
     end
+end
 
-    @test size(tmpvar(d).xbeta) == size(y(d))
+for data in (datavec, datavec_wtmpvar)
+    for (i,grp) in data
+        
+        obsptr = ptr(data)
+        obs = Observation(grp)
 
-    dtv = DataWithTmpVar(d)
-    for (i, otv) in enumerate(dtv)
-        @test data(otv) === getindex(d, i)
-        @test tmpvar(otv) === tmpvar(dtv)
+        @test idx = obsptr[i] : obsptr[i+1]-1
+        @test ptr(grp) == idx
+        @test obs == Observation(data,i)
+        @test y(obs) isa SubArray
+        @test y(obs) === view(y(data), idx)
+        @test x(obs) isa SubArray
+        @test x(obs) === view(x(data), :, idx)
+    
     end
+end
 
-    println(typeof(tmpvar(d)))
-# end
-
-# @testset "GroupedDataTest" begin
-    gptr = [1, 2, 2, 7]
-    tptr = [1, 2, 2, 5, 8, 9, 11]
-    n = last(tptr)-1
-    k = 2
-
-    d = GroupedDataTest(rand(n), rand(k,n), gptr, tptr)
-    # @test getindex(d,1) isa ObservationGroup
-    println(typeof(getindex(d,1)))
-
-    @test gptr === group_ptr(d)
-    @test tptr === obs_ptr(d)
-
-    @test grouprange(d,length(d)) == gptr[end-1]: gptr[end]-1
-    @test obsrange(d, 6) == 9:10
-
-    ti = 1
-    for (i,g) in enumerate(d)
-        @test grouprange(d,i) == gptr[i]: gptr[i+1]-1
-        for (j,o) in enumerate(g)
-            @test obsrange(g,j) == tptr[ti] : tptr[ti+1]-1
-            @test y(o) == y(d)[obsrange(g,j)]
-            ti += 1
+for data in (datavecbysca, datavecbysca_wtmpvar)
+    for (i,grp) in data
+        
+        obsptr = ptr(data)
+        idx = obsptr[i] : obsptr[i+1]-1
+        @test ptr(grp) == idx
+        
+        for (t,obs) in grp
+            @test obs == Observation(data,t)
+            @test y(obs) isa Number
+            @test y(obs) === getindex(y(data), t)
+            @test x(obs) isa SubArray
+            @test x(obs) === view(x(data), :, t)
         end
     end
+end
 
-    dtv = DataWithTmpVar(d)
-    dtvint = DataWithTmpVar(d, 1)
-    @assert eltype(dtv) == Float64 == eltype(tmpvar(dtv))
-    @assert eltype(dtvint) == Int == eltype(tmpvar(dtvint))
 
-    tmpv = tmpvar(dtv)
-    @test tmpv isa TmpVarTest
-    @test tmpvar(dtv) === TmpVarTest(tmpv)
-    @test tmpvar(dtv) === TmpVarTest(tmpvar(dtv))
 
-    @test getindex(tmpv, 1) == xbeta(tmpv)[1]
 
-    # for (i, gt) in enumerate(dtv)
-    #     @test tmpvar(gt) === tmpvar(dtv)
-        # @test getindex(dtv, i) == ObservationGroup(dtv,i)
-    # end
 
-# end
 
-jb = JnkBase{2,Int}(1)
-jg1 = JnkGroup(jb,1)
-jg2 = JnkGroup(jg1)
-jg3 = JnkGroup(jg2)
+
 
 # end # module
